dd4b305a1c294e06cb2dac1b21f6127eba0f2356
Rahul Jha <rahul.jha.stats@gmail.com>
2020-02-17 22:56:29 +0530

    Video 6

---
+++ Video - 6/main.py
@@ -0,0 +1,26 @@
+from textblob import TextBlob
+from flask import Flask, render_template
+
+def get_sentiment(text):
+    blob = TextBlob(text)
+    score = blob.sentiment.polarity
+    if score > 0:
+        if score > 0.5:
+            return "This sentence is highly positive."
+        else:
+            return "This sentence is positive."
+    elif score == 0:
+        return "This sentence is neutral."
+    else:
+        if score < -0.5:
+            return "This sentence is highly negative." # -0.9
+        else:
+            return "This sentence is negative." # -0.3
+
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug = True)
